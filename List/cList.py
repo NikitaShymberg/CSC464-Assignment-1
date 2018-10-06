909e134b38cf4cae2dd0db67c47fbd35a227bb5d
Nikita Shymberg <nikitashymberg@gmail.com>
2018-10-06 13:02:09 -0700

    List v1

--- List/cList.py
+++ List/cList.py
@@ -1,5 +1,7 @@
-from threading import Semaphore
+from threading import Semaphore, Thread
+from random import randrange
 
+# Concurrent list
 class cList:
     def __init__(self):
         self.rMutex = Semaphore()
@@ -20,5 +22,24 @@ class cList:
             self.rMutex.acquire()
             self.wMutex.acquire()
             self.list[index] = val
+            print(self.list)
             self.wMutex.release()
             self.rMutex.release()
+
+def reader(target, index):
+    print("Read", target.read(index), "From", index)
+
+def writer(target, index, val):
+    target.write(index, val)
+    print("Wrote", val, "To", index)
+
+l = cList()
+
+for i in range(5):
+    Thread(target=writer, args=(l, randrange(0, 10), randrange(10, 20))).start()
+
+for i in range(20):
+    Thread(target=reader, args=(l, randrange(0, 10),)).start()
+
+for i in range(5):
+    Thread(target=writer, args=(l, randrange(0, 10), randrange(10, 20))).start()
